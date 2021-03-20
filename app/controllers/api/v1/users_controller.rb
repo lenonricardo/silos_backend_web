b9bc90f50e354f5cce3eeb0d9b964dcd8b36702a
Lenon Mendes <lenonricardomendes@gmai.com>
2021-03-20 12:44:11 -0300

    Ajustes para validar usuário já cadastrado + Ajustes nas mensagens de retorno.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,34 +2,40 @@ module Api
 	module V1
 		class UsersController < ApplicationController
 			def index
-				user = User.order('created_at DESC').select('name', 'email', 'id');
+				user = User.order('created_at DESC').select('name', 'email', 'id', 'nivel', 'created_at', 'updated_at');
 				render json: {status: 'SUCCESS', message:'users carregadas', data:user},status: :ok
 			end
 
 			def show
-				user = User.where(["id = ?", params[:id]]).select("name, email, id").first
+				user = User.where(["id = ?", params[:id]]).select("name, email, id, nivel").first
 				render json: {status: 'SUCCESS', message:'Loaded user', data:user},status: :ok
 			end
 
 			def create
-				users = User.new(user_params)
-				if users.save
-					render json: {status: 'SUCCESS', message:'Usuário cadastrado com sucesso.', data:users},status: :ok
+				user = User.find_by_email(user_params[:email])
+
+				if !user
+					users = User.new(user_params)
+					if users.save
+						render json: {status: 'SUCCESS', message:'Usuário cadastrado com sucesso.', data:users},status: :ok
+					else
+						render json: {status: 'ERROR', message:'Ocorreu um erro ao gravar o cadastro.', data:users.errors},status: :unprocessable_entity
+					end
 				else
-					render json: {status: 'ERROR', message:'Ocorreu um erro ao gravar o cadastro.', data:users.errors},status: :unprocessable_entity
+					render json: {status: 'ERROR', message:'Usuário já cadastrado.' },status: :unprocessable_entity
 				end
 			end
 
 			def destroy
 				user = User.find(params[:id])
 				user.destroy
-				render json: {status: 'SUCCESS', message:'Deleted user', data:user},status: :ok
+				render json: {status: 'SUCCESS', message:'Usuário excluído com sucesso!', data:user},status: :ok
 			end
 
 			def update
 				user = User.find(params[:id])
 				if user.update(user_params)
-					render json: {status: 'SUCCESS', message:'Updated user', data:user},status: :ok
+					render json: {status: 'SUCCESS', message:'Usuário alterado com sucesso', data:user},status: :ok
 				else
 					render json: {status: 'ERROR', message:'users not update', data:user.erros},status: :unprocessable_entity
 				end
